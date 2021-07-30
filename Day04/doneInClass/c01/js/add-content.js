12c6ddb3a6c1cddaab36629225c952067c0e2bbe
Trish Ladd <4132874+TrishZwei@users.noreply.github.com>
2021-07-30 10:48:31 -0700

    updates day 4

---
+++ Day04/doneInClass/c01/js/add-content.js
@@ -0,0 +1,19 @@
+//convert to class demo method, eliminating the bad practice of document.write
+
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning!';
+} else {
+    greeting = 'Welcome!';
+}
+
+document.write('<h3>' + greeting + '</h3>');
+
+
